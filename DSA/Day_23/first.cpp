e00415097afbf8b3ec32832f7a98f2cb3197753f
Yashswi-3 <141171252+Yashswi-3@users.noreply.github.com>
2024-07-08 14:40:13 +0000

    Final

---
+++ DSA/Day_23/first.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
+
+int main() {
+
+    int num ; 
+    cout << "Enter num : " ;
+    cin >> num ;
+    bool isPow = true ;
+
+    
+    for(int i=0 ; i < 31 ; i++){
+    
+        if (pow(2,i)==num){
+            isPow = true ;
+            break;
+        }
+        else {
+            isPow = false ;
+        }
+    }
+
+    if (isPow){
+        cout << "True" ;
+    }
+    else {
+        cout << "False" ;
+    }
+
+}
