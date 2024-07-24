8a7bf03cde022697a404e7aa80d5f409d22064c0
Yashswi-3 <141171252+Yashswi-3@users.noreply.github.com>
2024-07-24 16:45:52 +0000

    FInal

---
+++ DSA/Day_37/second.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 2 ," << endl ;
+    cout << "Day Left - 363 ," << endl ;
+    cout << "I am Concisious today! ," << endl ;
+    cout << "Still i haven't found the answer Why? ," << endl ;
+}
