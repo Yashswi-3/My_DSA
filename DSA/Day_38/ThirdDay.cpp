6fd9ae6ae16faedac142b8155c66781604749079
Yashswi-3 <141171252+Yashswi-3@users.noreply.github.com>
2024-07-25 16:57:43 +0000

    Final

---
+++ DSA/Day_38/ThirdDay.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 3 ," << endl ;
+    cout << "Day Left - 362 ," << endl ;
+    cout << "I was Unconcisious today! ," << endl ;
+    cout << "I didn't think about anything! ," << endl ;
+}
