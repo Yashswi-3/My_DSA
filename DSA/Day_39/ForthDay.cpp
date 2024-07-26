6aea6b8a7aa5d5830fd629d05520d1ede067695c
Yashswi-3 <141171252+Yashswi-3@users.noreply.github.com>
2024-07-26 16:51:07 +0000

    Final

---
+++ DSA/Day_39/ForthDay.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 4 ," << endl ;
+    cout << "Day Left - 361 ," << endl ;
+    cout << "I was Unconcisious today! ," << endl ;
+    cout << "I didn't think about anything! ," << endl ;
+    cout << "Hff today i was totally out of it." << endl ;
+}
