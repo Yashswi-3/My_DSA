a4ff1f9f213f304ddd85edf42f7ab7e20efc4b08
Yashswi-3 <141171252+Yashswi-3@users.noreply.github.com>
2024-07-27 18:20:06 +0000

    FInal

---
+++ DSA/Day_40/FifthDay.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 5 ," << endl ;
+    cout << "Day Left - 360 ," << endl ;
+    cout << "I was Unconcisious today! ," << endl ;
+    cout << "I didn't think about anything! ," << endl ;
+    cout << "Today was the last day of vacation, Tommorow is the start of the college 3rd year!." << endl ;
+}
