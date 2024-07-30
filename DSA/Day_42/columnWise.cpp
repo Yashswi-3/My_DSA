3bac2677bd8e748d8dafa8b45938963924fb1f32
Yashswi-3 <yashswishukla@gmail.com>
2024-07-30 20:16:19 +0530

    Second

---
+++ DSA/Day_42/columnWise.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void rowWiseSum(int arr[][4], int m, int n){
+
+    int sum = 0 ;
+    for(int i = 0 ; i < 4 ; i++ ){
+        for(int j=0 ; j < 3 ; j++) {
+            sum += arr[j][i] ;
+        }
+        cout << "Sun of column "<< i << " is : " << sum << endl ;
+        sum = 0 ;
+    }
+}
+
+int main() {
+
+    int arr[3][4] ; 
+
+    cout << "Enter the array : " << endl ; 
+
+    for (int row=0 ; row < 3 ; row++){
+        for(int col = 0 ; col < 4 ; col++){
+            cin >> arr[row][col] ;
+        }
+    } 
+
+    cout << endl << "The input array is : " << endl ;
+
+    for (int i=0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 4 ; j++){
+            cout << arr[i][j] << " " ;
+        }
+        cout << endl ; 
+    } 
+
+    rowWiseSum( arr, 3 , 4) ;
+
+}
