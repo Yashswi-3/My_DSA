8a5bc1477dcd2040c1084d398bd25cbb50b350c1
Yashswi-3 <yashswishukla@gmail.com>
2024-07-31 18:13:05 +0530

    Second

---
+++ DSA/Day_43/SeventhDay.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 7 ," << endl ;
+    cout << "Day Left - 358 ," << endl ;
+    cout << "I was Concisious today! ," << endl ;
+    cout << "I didn't think about the why but i have the prsence of mind ," << endl ;
+}
