1aeb072a4170f7dca58f3dda48555b81907459d5
Yashswi-3 <yashswishukla@gmail.com>
2024-08-03 23:25:57 +0530

    Final

---
+++ DSA/Day_46/TenthDay.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "Day Done - 10 ," << endl ;
+    cout << "Day Left - 355 ," << endl ;
+    cout << "I was Concisious today! ," << endl ;
+    cout << "DSA - Did Today ," << endl ;
+    cout << "Machine learning - Not Done," << endl ;
+    cout << "Project - Not Done ." << endl ;
+}
