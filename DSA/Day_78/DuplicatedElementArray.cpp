832706ff271758a4da0f16600625403338009d98
Yashswi-3 <yashswishukla@gmail.com>
2025-01-05 18:52:08 +0530

    final

--- DSA/Day_78/DuplicatedElementArray.cpp
+++ DSA/Day_78/DuplicatedElementArray.cpp
@@ -5,35 +5,31 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 0) return 0; // If the array is empty, return 0
+        if (nums.size() == 0) return 0; 
 
-        int uniqueIndex = 0; // Pointer to place unique elements
+        int uniqueIndex = 0; 
 
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] != nums[uniqueIndex]) {
-                uniqueIndex++; // Move the unique pointer
-                nums[uniqueIndex] = nums[i]; // Update the value at uniqueIndex
+                uniqueIndex++; 
             }
         }
 
-        return uniqueIndex + 1; // The count of unique elements
+        return uniqueIndex + 1; 
     }
 };
 
 int main() {
     Solution ob;
 
-    // Example inputs
     vector<int> nums1 = {1, 1, 2};
     vector<int> nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
 
-    // Test case 1
     int k1 = ob.removeDuplicates(nums1);
     cout << "k = " << k1 << ", nums = [";
     for (int i = 0; i < k1; ++i) cout << nums1[i] << (i < k1 - 1 ? ", " : "");
     cout << "]" << endl;
 
-    // Test case 2
     int k2 = ob.removeDuplicates(nums2);
     cout << "k = " << k2 << ", nums = [";
     for (int i = 0; i < k2; ++i) cout << nums2[i] << (i < k2 - 1 ? ", " : "");
