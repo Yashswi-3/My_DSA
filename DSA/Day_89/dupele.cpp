b6f9e5fe36c11510afd5d32f517b028afe270c0a
Yashswi-3 <yashswishukla@gmail.com>
2025-02-06 21:58:10 +0530

    First

--- DSA/Day_89/dupele.cpp
+++ DSA/Day_89/dupele.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int unique(vector<int> arr){
     int n = arr.size();
-    for(int i=0; i<n ; i++){
-        if ( arr[i] == arr[i+1] ){
-            n--;
+    int k = 0 ;
+    for(int i=0 ; i<n ; i++){
+        if (arr[i]!=arr[k]){
+            k++;
+            arr[k]=arr[i];
         }
     }
-    return n ;
+    return k+1 ;    
+
 }
 
 vector<int> dup(vector<int> arr){
@@ -19,5 +22,9 @@ int main(){
 
     vector<int> arr = {1,2,3,4,4,5,5} ;
     cout << unique(arr) << endl ; 
+
+    for(int i=0 ; i<arr.size() ; i++){
+        cout << arr[i] << " " ;
+    }
 
 }
