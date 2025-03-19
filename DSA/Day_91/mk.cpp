b741461f561c455c4cd452150c82f0c8bd1444f7
Yashswi-3 <yashswishukla@gmail.com>
2025-03-19 20:55:12 +0530

    Final

--- DSA/Day_91/mk.cpp
+++ DSA/Day_91/mk.cpp
@@ -4,33 +4,46 @@ using namespace std;
 
 class Solution {
 public:
-    int longestNiceSubarray(vector<int>& nums) {
-        int maxLen = 0, mask = 0;
-        int left = 0;
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int operations = 0;
 
-        for (int right = 0; right < nums.size(); ++right) {
-            while ((mask & nums[right]) != 0) {
-                mask ^= nums[left];   // Remove the leftmost element
-                left++;
+        // Iterate over the array
+        for (int i = 0; i <= n - 3; i++) {
+            if (nums[i] == 0) {
+                // Flip current group of 3
+                nums[i] = 1 - nums[i];
+                nums[i + 1] = 1 - nums[i + 1];
+                nums[i + 2] = 1 - nums[i + 2];
+                operations++;
             }
-            
-            mask |= nums[right];      // Add the current number
-            maxLen = max(maxLen, right - left + 1);
         }
 
-        return maxLen;
+        // Check if the entire array contains only 1s
+        for (int num : nums) {
+            if (num == 0) return -1;
+        }
+
+        return operations;
     }
 };
+
 
 int main() {
     Solution sol;
 
-    vector<int> nums1 = {1, 3, 8, 48, 10};
-    cout << sol.longestNiceSubarray(nums1) << endl;  
+    vector<int> nums1 = {0, 1, 1, 1, 0, 0};
+    cout << sol.minOperations(nums1) << endl;  // Output: 3
 
-    vector<int> nums2 = {3, 1, 5, 11, 13};
-    cout << sol.longestNiceSubarray(nums2) << endl;  
+    vector<int> nums2 = {0, 1, 1, 1};
+    cout << sol.minOperations(nums2) << endl;  // Output: -1
 
-    vector<int> nums3 = {1, 2, 4, 8, 16};
-    cout << sol.longestNiceSubarray(nums3) << endl;  
+    vector<int> nums3 = {1, 0, 0, 0, 1, 1, 0, 0, 0};
+    cout << sol.minOperations(nums3) << endl;  // Output: 3
+
+    vector<int> nums4 = {1, 1, 1, 1};
+    cout << sol.minOperations(nums4) << endl;  // Output: 0 (already all 1s)
+
+    vector<int> nums5 = {0, 0, 0, 0, 0, 0};
+    cout << sol.minOperations(nums5) << endl;  // Output: 2
 }
