511b56991015dc1656d64c0684828f46dcdac5a5
Yashswi-3 <yashswishukla@gmail.com>
2025-03-20 20:23:43 +0530

    Final

--- DSA/Day_91/findele.cpp
+++ DSA/Day_91/findele.cpp
@@ -1,29 +1,61 @@
-#include<bits/stdc++.h>
+#include <vector>
 using namespace std;
 
-int missingNumber(vector<int>& arr) {
+class DSU {
+public:
+    vector<int> parent, rank, and_val;
     
-    int n = arr.size();
-    for(int i=1 ; i<n ; i++){
-        bool flag = 0 ;
-        for(int j=0 ; j<n ; j++){
-            if(arr[j]==i) flag = 1 ;
-        }
-        if(flag == 0) return i;
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+        and_val.resize(n, ~0); // Initialize with all bits set to 1
+        for (int i = 0; i < n; ++i)
+            parent[i] = i;
     }
     
-}
-
-int hashmiss(vector<int>& arr){
-    int n = arr.size();
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
     
+    void unite(int u, int v, int w) {
+        int root_u = find(u);
+        int root_v = find(v);
+        if (root_u == root_v) {
+            and_val[root_u] &= w;
+            return;
+        }
+        // Union by rank
+        if (rank[root_u] < rank[root_v]) {
+            swap(root_u, root_v);
+        }
+        parent[root_v] = root_u;
+        and_val[root_u] &= (and_val[root_v] & w);
+        if (rank[root_u] == rank[root_v])
+            rank[root_u]++;
+    }
+};
 
-}
-
-int main(){
-
-    vector<int> num = {1,3,0} ;
-
-    cout << missingNumber(num) ;
-
-}
+class Solution {
+public:
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        DSU dsu(n);
+        for (auto& e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            dsu.unite(u, v, w);
+        }
+        vector<int> res;
+        for (auto& q : query) {
+            int s = q[0], t = q[1];
+            int root_s = dsu.find(s);
+            int root_t = dsu.find(t);
+            if (root_s != root_t) {
+                res.push_back(-1);
+            } else {
+                res.push_back(dsu.and_val[root_s]);
+            }
+        }
+        return res;
+    }
+};
