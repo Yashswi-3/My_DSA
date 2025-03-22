a50dea627442c4f06f3a33985d90b80fb1a00896
Yashswi-3 <yashswishukla@gmail.com>
2025-03-22 20:47:37 +0530

    Final

--- DSA/Day_91/nnnnmj.cpp
+++ DSA/Day_91/nnnnmj.cpp
@@ -1,21 +1,60 @@
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    bool divideArray(vector<int>& nums) {
-        int freq[501] = {0}; // Fixed-size frequency array
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<bool> visited(n, false);
 
-        // Count occurrences
-        for (int num : nums) {
-            freq[num]++;
+        // Build adjacency list
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
 
-        // Check if all counts are even
-        for (int i = 1; i <= 500; i++) {
-            if (freq[i] % 2 != 0) return false;
+        int completeCount = 0;
+
+        // BFS to traverse components
+        for (int i = 0; i < n; ++i) {
+            if (visited[i]) continue;
+
+            queue<int> q;
+            q.push(i);
+            visited[i] = true;
+
+            unordered_set<int> component;
+            component.insert(i);
+
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for (int neighbor : adj[node]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        q.push(neighbor);
+                        component.insert(neighbor);
+                    }
+                }
+            }
+
+            // Check if the component is complete
+            bool isComplete = true;
+            int size = component.size();
+            
+            for (int node : component) {
+                if (adj[node].size() != size - 1) {
+                    isComplete = false;
+                    break;
+                }
+            }
+
+            if (isComplete) {
+                completeCount++;
+            }
         }
 
-        return true;
+        return completeCount;
     }
 };
