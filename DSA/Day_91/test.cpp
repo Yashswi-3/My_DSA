e83407aadece054f05f939c6a53a2ecccb3596ea
Yashswi-3 <yashswishukla@gmail.com>
2025-03-23 23:54:50 +0530

    Final

--- DSA/Day_91/test.cpp
+++ DSA/Day_91/test.cpp
@@ -1,30 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int  main(){
-    int x = 10;
-    int *ptr = &x;
-    int **ptr1 = &ptr;  // 'ptr' holds the address of 'x'
-    cout << "Address of x: " << &x << endl;
-    cout << "Value of x using pointer: " << *ptr << endl;
-    cout << "Value of x without using pointer: " << ptr << endl;
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        const int MOD = 1e9 + 7;
 
-    cout << "Value of x using ** pointer: " << *ptr1 << endl;
+        // Graph representation: adjacency list with {neighbor, time}
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& road : roads) {
+            adj[road[0]].emplace_back(road[1], road[2]);
+            adj[road[1]].emplace_back(road[0], road[2]);
+        }
 
+        // Dijkstra's Algorithm
+        vector<long long> minTime(n, LLONG_MAX);
+        vector<long long> ways(n, 0);
 
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+        pq.emplace(0, 0);
+        minTime[0] = 0;
+        ways[0] = 1;
 
-//     int arr[] = {10, 20, 30};
-//     int *p = arr;  // Points to arr[0]
+        while (!pq.empty()) {
+            auto [time, node] = pq.top();
+            pq.pop();
 
-//     cout << *p << endl;   // 10
-//     p++;                 // Move to next element
-//     cout << *p << endl;   // 20
-//     *p++;
-//     cout << *p << endl; 
-//     *p++;
-//     cout << *p << endl;   // 20
-//   // 20
+            if (time > minTime[node]) continue;
 
+            for (auto& [neighbor, t] : adj[node]) {
+                long long newTime = time + t;
 
+                if (newTime < minTime[neighbor]) {
+                    minTime[neighbor] = newTime;
+                    ways[neighbor] = ways[node];
+                    pq.emplace(newTime, neighbor);
+                } 
+                else if (newTime == minTime[neighbor]) {
+                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD;
+                }
+            }
+        }
 
-}
+        return ways[n - 1];
+    }
+};
